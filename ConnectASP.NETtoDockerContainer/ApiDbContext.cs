e29e46423736d3ca99b3c39dfeed2664521cb5d1
BeginnerDuelist <catalinbuza31@gmail.com>
2023-12-11 20:25:45 +0200

    Add project files.

---
+++ ConnectASP.NETtoDockerContainer/ApiDbContext.cs
@@ -0,0 +1,9 @@
+﻿using ConnectASP.NETtoDockerContainer;
+using Microsoft.EntityFrameworkCore;
+
+public class ApiDbContext:DbContext
+{
+     public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+     public DbSet<Student>   Students { get; set; }
+
+}
---
+++ ConnectASP.NETtoDockerContainer/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectASP.NETtoDockerContainer
+{
+     public class Student
+     {
+          public int Id { get; set; }
+          public string Name { get; set; }
+
+     }
+}
